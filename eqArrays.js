85fa801d892a79d94203c31c7061cf2ec1d4646e
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-02-09 03:19:33 +0000

    corrected code with added test

--- eqArrays.js
+++ eqArrays.js
@@ -12,15 +12,17 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   let boolOutput = false;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      boolOutput = true;
-    } else {
-      boolOutput = false;
+  if (array1.length === array2.length){
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        boolOutput = true;
+      } else {
+        boolOutput = false;
+      }
     }
   }
-  return boolOutput;
-};
+    return boolOutput;
+  };
 
 
 
@@ -29,3 +31,4 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2"]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false);
