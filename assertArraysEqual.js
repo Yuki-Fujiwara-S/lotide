354b810b3f6e2f9949d254a54dd4ee6d70c5db4a
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-02-09 03:23:20 +0000

    ESLint edits

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,6 @@
 const eqArrays = function(array1, array2) {
   let boolOutput = false;
-  if (array1.length === array2.length){
+  if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] === array2[i]) {
         boolOutput = true;
@@ -9,8 +9,8 @@ const eqArrays = function(array1, array2) {
       }
     }
   }
-    return boolOutput;
-  };
+  return boolOutput;
+};
 
 
 const assertArraysEqual = function(array1, array2) {
