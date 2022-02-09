2abe1776e67fc656be91255b3997aa93c52e4274
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-02-09 22:37:01 +0000

    added another test

--- without.js
+++ without.js
@@ -43,6 +43,7 @@ const without = function(source, itemsToRemove) {
 assertArraysEqual(without([1, 2, 3], [1, 2]), [3]);
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1","2"]);
 assertArraysEqual(without([1, 2, 3], [2, 1]), [3]);
+assertArraysEqual(without([1,2], [1,2]), []);
 
 
 
