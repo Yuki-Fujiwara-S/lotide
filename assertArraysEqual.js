a00f8014fa00fac39d85ee6cd72bd1beb8faf9fd
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-02-11 05:03:34 +0000

    Refactored to remove else, make cleaner

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,17 @@
-const eqArrays = function(array1, array2) {
-  let boolOutput = false;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        boolOutput = true;
-      } else {
-        boolOutput = false;
-      }
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
-  if (array1.length === 0 && array2.length === 0) {
-    boolOutput = true;
-  }
-  return boolOutput;
+  return true;
 };
 
 
-const assertArraysEqual = function(array1, array2) {
+const assertArraysEqual = function (array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`✅The arrays are equal!✅ : ${array1} === ${array2}`);
   } else {
@@ -25,7 +20,7 @@ const assertArraysEqual = function(array1, array2) {
 };
 
 
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1,2,3], [1,2,"3"]);
-assertArraysEqual([1,2,3], [1,2,3,4]);
-assertArraysEqual([1,2,3], [3,2,1]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
