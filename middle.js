0d09057597300abd5b36d4aae6eaeb1d2340213e
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-02-15 05:09:23 +0000

    removed node modules  and gitignore

--- middle.js
+++ middle.js
@@ -1,4 +1,3 @@
-const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = function(array) {
   let midOutput = [];
--- test/middleTest.js
+++ test/middleTest.js
@@ -25,20 +25,7 @@ describe("#middle", () => {
   it("returns [3,4] for [1,2,3,4,5,6]", () => {
     assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]);
   });
-  
+
 });
 
 
-/*
-//No middle tests
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1,2]), []);
-
-//Odd tests
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-
-//Even tests
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
-*/
