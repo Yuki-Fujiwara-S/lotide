873f66f716917ecc1165db36d2c8e7430af0ec1a
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-02-15 00:02:54 +0000

    refactored and tested eqArrays test

--- eqArrays.js
+++ eqArrays.js
@@ -1,37 +1,17 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 // It's okay for eqArrays to not return true for nested arrays or arrays of objects that are identical.
 // We will save this "deeper" problem for another day.
 
-const eqArrays = function(array1, array2) {
-  let boolOutput = false;
-  if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-        boolOutput = true;
-      } else {
-        boolOutput = false;
-      }
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   }
-  if (array1.length === 0 && array2.length === 0) {
-    boolOutput = true;
-  }
-  return boolOutput;
+  return true;
 };
 
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2"]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", "4"]), false);
+module.exports = eqArrays;
