e9befb09adb88d78d20d0f3709a36f031e4d817d
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-02-15 03:32:30 +0000

    refactored to property value short hand syntax from ES6

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ const map = require('./map');
 
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  letterPositions: letterPositions,
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
   map: map
 };
