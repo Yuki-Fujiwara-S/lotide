bef46ecf3060e630aebaad93cf9f019cc7dafc77
Sathvik Yuki Fujiwara <sathvikyf@gmail.com>
2022-02-15 00:08:40 +0000

    refactored and created seperate test file for assertarraysEqual

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require("../assertArraysEqual")
+
+
+//Tests
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
